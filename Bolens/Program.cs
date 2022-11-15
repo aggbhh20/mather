7cb8e027f1f44be0912f51b4f3c93e0fcb24ec21
aggbhh20 <aggbhh4@gmail.com>
2022-11-15 17:21:51 +0300

    Add project files.

---
+++ Bolens/Program.cs
@@ -0,0 +1,44 @@
+﻿using Bolens;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace Bolens
+{
+    
+    public class Program
+    {
+
+        
+        static void exit()
+        {
+            System.Environment.Exit(0);
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string? userInput;
+                Console.Write("mather>");
+                userInput = Console.ReadLine();
+                if (userInput == "bölenler")
+                {
+                    boling.bolens();
+                }
+                else if (userInput == "exit")
+                {
+                    exit();
+                }
+                else if (userInput == null)
+                {
+                    Console.WriteLine("you didnt type anything!");
+                }
+                else
+                {
+                    Console.WriteLine("ü");
+                }
+                
+            }
+            
+        }
+    }
+}
---
+++ Bolens/boling.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolens
+{
+    internal class boling
+    {
+        public static void bolens()
+        {
+            int i;
+            i = 1;
+            Console.Write("num 1>");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("num2>");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                while (i < b)
+                {
+                    if (a % i == 0 && b % i == 0)
+                    {
+                        Console.WriteLine(i);
+                        i++;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+            else if (b > a)
+            {
+
+                while (i < a)
+                {
+                    if (a % i == 0 && b % i == 0)
+                    {
+                        Console.WriteLine(i);
+                        i++;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
